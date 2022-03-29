19fae880c59b515eea5182534998a7ec6f9c41ce
Mark <carter.m@northeastern.edu>
2022-03-29 15:14:56 -0400

    Adding dislikes features

---
+++ src/services/dislikes-service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+//const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = 'https://cs5500-02.herokuapp.com';
+const USERS_API = `${BASE_URL}/api/users`;
+const TUITS_API = `${BASE_URL}/api/tuits`;
+
+const api = axios.create({
+    withCredentials: true
+});
+
+export const findAllTuitsDisLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/dislikes`)
+        .then(response => response.data);
+
+export const findAllUsersThatDisLikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/dislikes`)
+        .then(response => response.data);
+
+export const userDisLikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+        .then(response => response.data);
