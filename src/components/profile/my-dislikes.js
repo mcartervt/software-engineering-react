15dc6fa96514f65d3a4a68709bd48a4b9eaa5419
Mark <carter.m@northeastern.edu>
2022-04-01 15:58:52 -0400

    Updated dislikes-service path in my-dislikes

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,5 +1,5 @@
 import Tuits from "../tuits";
-import * as service from "./dislikes-service";
+import * as service from "../../services/dislikes-service";
 import {useEffect, useState} from "react";
 
 const MyDislikes = () => {
